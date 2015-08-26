fb150708558569e19a3dfce6df9e0a42dc5c0a02
jsmunroe <jsmunroe@gmail.com.com>
2015-08-26 09:18:08 -0500

    Unit tests for ControllerBase.

--- SimpleMvc.Test/TestControllers/TestController.cs
+++ SimpleMvc.Test/TestControllers/TestController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpleMvc.Results;
 using SimpleMvc.Test.TestViews.TestController;
 
@@ -30,47 +32,261 @@ namespace SimpleMvc.Test.TestControllers
         }
 
 
+
+
+        // Tests of ControllerBase abstract class.
+
         [TestMethod]
         public void CallView()
         {
-            Assert.Inconclusive("Not implemented!");
+            // Execute
+            var viewResult = View();
+
+            // Assert
+            Assert.AreEqual(nameof(CallView), viewResult.ViewName);
+            Assert.IsNull(viewResult.Model);
         }
 
+        [TestMethod]
+        public void CallViewWithViewName()
+        {
+            // Setup
+            var testModel = new TestModel();
+
+            // Execute
+            var viewResult = View("ThisView", testModel);
+
+            // Assert
+            Assert.AreEqual("ThisView", viewResult.ViewName);
+            Assert.AreSame(testModel, viewResult.Model);
+        }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void CallViewWithNullViewName()
         {
-            Assert.Inconclusive("Not implemented!");
+            // Setup
+            var testModel = new TestModel();
+
+            // Execute
+            var viewResult = View(a_viewName: null, a_model: testModel);
         }
 
 
         [TestMethod]
         public void CallViewWithNullModel()
         {
-            Assert.Inconclusive("Not implemented!");
+            // Execute
+            var viewResult = View(a_viewName: "ViewName", a_model: null);
+
+            // Assert
+            Assert.AreEqual("ViewName", viewResult.ViewName);
+            Assert.IsNull(viewResult.Model);
         }
 
 
         [TestMethod]
+        public void CallRedirect()
+        {
+            // Setup
+            var values = new RouteDictionary();
+            values["One"] = 1;
+            values["Two"] = 2;
+
+            // Execute
+            var result = Redirect("ActionName", values);
+
+            // Assert
+            Assert.IsNull(result.ControllerName);
+            Assert.AreEqual("ActionName", result.ActionName);
+
+            Assert.IsNotNull(result.Values);
+            Assert.IsTrue(result.Values.ContainsKey("One"));
+            Assert.AreEqual(1, result.Values["One"]);
+            Assert.IsTrue(result.Values.ContainsKey("Two"));
+            Assert.AreEqual(2, result.Values["Two"]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void CallRedirectWithNullActionName()
         {
-            Assert.Inconclusive("Not implemented!");
+            // Setup
+            var values = new RouteDictionary();
+            values["One"] = 1;
+            values["Two"] = 2;
+
+            // Execute
+            var result = Redirect(a_actionName: null, a_values: values);
         }
-
-
-        [TestMethod]
-        public void CallRedirectWithNullControllerName()
-        {
-            Assert.Inconclusive("Not implemented!");
-        }
-
 
         [TestMethod]
         public void CallRedirectWithNullValues()
         {
-            Assert.Inconclusive("Not implemented!");
+            // Execute
+            var result = Redirect(a_actionName: "ActionName", a_values: null);
+
+            // Assert
+            Assert.IsNull(result.ControllerName);
+            Assert.AreEqual("ActionName", result.ActionName);
+
+            Assert.IsNotNull(result.Values);
+            Assert.IsFalse(result.Values.Any());
         }
 
+        [TestMethod]
+        public void CallRedirectWithControllerName()
+        {
+            // Setup
+            var values = new RouteDictionary();
+            values["One"] = 1;
+            values["Two"] = 2;
 
+            // Execute
+            var result = Redirect(a_actionName: "ActionName",a_controllerName: "Controller",  a_values: values);
+
+            // Assert
+            Assert.AreEqual("Controller", result.ControllerName);
+            Assert.AreEqual("ActionName", result.ActionName);
+
+            Assert.IsNotNull(result.Values);
+            Assert.IsTrue(result.Values.ContainsKey("One"));
+            Assert.AreEqual(1, result.Values["One"]);
+            Assert.IsTrue(result.Values.ContainsKey("Two"));
+            Assert.AreEqual(2, result.Values["Two"]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CallRedirectWithNullControllerName()
+        {
+            // Setup
+            var values = new RouteDictionary();
+            values["One"] = 1;
+            values["Two"] = 2;
+            
+            // Execute
+            var result = Redirect(a_actionName: "ActionName",a_controllerName: null,  a_values: values);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CallRedirectWithControllerNameAndNullActionName()
+        {
+            // Setup
+            var values = new RouteDictionary();
+            values["One"] = 1;
+            values["Two"] = 2;
+
+            // Execute
+            var result = Redirect(a_actionName: null, a_controllerName: "Controller", a_values: values);
+        }
+
+        [TestMethod]
+        public void CallRedirectWithControllerNameAndNullValues()
+        {
+            // Execute
+            var result = Redirect(a_actionName: "ActionName", a_controllerName: "Controller", a_values: null);
+
+            // Assert
+            Assert.AreEqual("Controller", result.ControllerName);
+            Assert.AreEqual("ActionName", result.ActionName);
+
+            Assert.IsNotNull(result.Values);
+            Assert.IsFalse(result.Values.Any());
+        }
+
+        [TestMethod]
+        public void CallRedirectWithDynamicValues()
+        {
+            // Setup
+            dynamic values = new
+            {
+                One = 1,
+                Two = 2,
+            };
+
+            // Execute
+            var result = Redirect("ActionName", values);
+
+            // Assert
+            Assert.IsNull(result.ControllerName);
+            Assert.AreEqual("ActionName", result.ActionName);
+
+            Assert.IsNotNull(result.Values);
+            Assert.IsTrue(result.Values.ContainsKey("One"));
+            Assert.AreEqual(1, result.Values["One"]);
+            Assert.IsTrue(result.Values.ContainsKey("Two"));
+            Assert.AreEqual(2, result.Values["Two"]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CallRedirectWithDynamicValuesAndNullActionName()
+        {
+            // Setup
+            dynamic values = new
+            {
+                One = 1,
+                Two = 2,
+            };
+
+            // Execute
+            var result = Redirect(a_actionName: null, a_values: values);
+        }
+
+        [TestMethod]
+        public void CallRedirectWithDynamicValuesAndControllerName()
+        {
+            // Setup
+            dynamic values = new
+            {
+                One = 1,
+                Two = 2,
+            };
+
+            // Execute
+            var result = Redirect(a_actionName: "ActionName", a_controllerName: "Controller", a_values: values);
+
+            // Assert
+            Assert.AreEqual("Controller", result.ControllerName);
+            Assert.AreEqual("ActionName", result.ActionName);
+
+            Assert.IsNotNull(result.Values);
+            Assert.IsTrue(result.Values.ContainsKey("One"));
+            Assert.AreEqual(1, result.Values["One"]);
+            Assert.IsTrue(result.Values.ContainsKey("Two"));
+            Assert.AreEqual(2, result.Values["Two"]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CallRedirectWithDynamicValuesAndNullControllerName()
+        {
+            // Setup
+            dynamic values = new
+            {
+                One = 1,
+                Two = 2,
+            };
+
+            // Execute
+            var result = Redirect(a_actionName: "ActionName", a_controllerName: null, a_values: values);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CallRedirectWithDynamicValuesAndControllerNameAndNullActionName()
+        {
+            // Setup
+            dynamic values = new
+            {
+                One = 1,
+                Two = 2,
+            };
+
+            // Execute
+            var result = Redirect(a_actionName: null, a_controllerName: "Controller", a_values: values);
+        }
     }
 }
--- SimpleMvc/ControllerBase.cs
+++ SimpleMvc/ControllerBase.cs
@@ -17,8 +17,16 @@ namespace SimpleMvc
         /// <param name="a_viewName">View name.</param>
         /// <param name="a_model">View model.</param>
         /// <returns>Created view result.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_viewName"/> is null.</exception>
         protected ViewResult View([CallerMemberName]string a_viewName = "", object a_model = null)
         {
+            #region Argument Validation
+
+            if (a_viewName == null)
+                throw new ArgumentNullException(nameof(a_viewName));
+
+            #endregion
+
             return new ViewResult
             {
                 ViewName = a_viewName,
@@ -32,12 +40,20 @@ namespace SimpleMvc
         /// <param name="a_actionName">Action name.</param>
         /// <param name="a_values">Route values.</param>
         /// <returns>Created redirect result.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_actionName"/> is null.</exception>
         protected RediectResult Redirect(string a_actionName, RouteDictionary a_values = null)
         {
+            #region Argument Validation
+
+            if (a_actionName == null)
+                throw new ArgumentNullException(nameof(a_actionName));
+
+            #endregion
+
             return new RediectResult
             {
                 ActionName = a_actionName,
-                Values = a_values,
+                Values = a_values ?? new RouteDictionary(),
             };
         }
 
@@ -47,8 +63,16 @@ namespace SimpleMvc
         /// <param name="a_actionName">Action name.</param>
         /// <param name="a_values">Route values.</param>
         /// <returns>Created redirect result.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_actionName"/> is null.</exception>
         protected RediectResult Redirect(string a_actionName, dynamic a_values = null)
         {
+            #region Argument Validation
+
+            if (a_actionName == null)
+                throw new ArgumentNullException(nameof(a_actionName));
+
+            #endregion
+
             return new RediectResult
             {
                 ActionName = a_actionName,
@@ -63,13 +87,26 @@ namespace SimpleMvc
         /// <param name="a_controllerName">Controller name.</param>
         /// <param name="a_values">Route values.</param>
         /// <returns>Created redirect result.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_actionName"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_controllerName"/> is null.</exception>
         protected RediectResult Redirect(string a_actionName, string a_controllerName, RouteDictionary a_values = null)
         {
+            #region Argument Validation
+
+            if (a_actionName == null)
+                throw new ArgumentNullException(nameof(a_actionName));
+
+            if (a_controllerName == null)
+                throw new ArgumentNullException(nameof(a_controllerName));
+
+            #endregion
+
+
             return new RediectResult
             {
                 ControllerName = a_controllerName,
                 ActionName = a_actionName,
-                Values = a_values,
+                Values = a_values ?? new RouteDictionary(),
             };
         }
 
@@ -80,8 +117,20 @@ namespace SimpleMvc
         /// <param name="a_controllerName">Controller name.</param>
         /// <param name="a_values">Route values.</param>
         /// <returns>Created redirect result.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_actionName"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="a_controllerName"/> is null.</exception>
         protected RediectResult Redirect(string a_actionName, string a_controllerName, dynamic a_values = null)
         {
+            #region Argument Validation
+
+            if (a_actionName == null)
+                throw new ArgumentNullException(nameof(a_actionName));
+
+            if (a_controllerName == null)
+                throw new ArgumentNullException(nameof(a_controllerName));
+
+            #endregion
+
             return new RediectResult
             {
                 ControllerName = a_controllerName,
